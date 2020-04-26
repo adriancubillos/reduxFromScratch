540d4f221ba390ac2492bba752f3e1dd1d725050
Adrian <adriancubillos@gmail.com>
2020-04-26 02:35:03 -0400

    Restructirung the bugs slice from array to object

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,8 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
-import { userAdded } from './users';
 
-const initialState = [];
+const initialState = {
+  list: [],
+  loading: false,
+  lastFetch: null
+};
 let lastId = 0;
 
 const slice = createSlice({
@@ -10,42 +13,47 @@ const slice = createSlice({
   initialState,
   reducers: {
     bugAdded: (bugs, action) => {
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description + '-' + lastId,
         resolved: false
       });
     },
     bugRemoved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-      bugs.splice(index, 1);
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list.splice(index, 1);
     },
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].resolved = true;
     },
     bugAssignedToUser: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].userId = action.payload.userId;
     }
   }
 });
 // object destructuring to export the action creators
-export const { bugAdded, bugResolved, bugRemoved, bugAssignedToUser } = slice.actions;
+export const {
+  bugAdded,
+  bugResolved,
+  bugRemoved,
+  bugAssignedToUser
+} = slice.actions;
 export default slice.reducer;
 
 // Selector function
 // instead of accessing the store and performing queries against it
-// this was form index.js ==> const unresolvedBugs = store.getState().entities.bugs.filter((bug) => !bug.resolved);
+// this was form index.js ==> const unresolvedBugs = store.getState().entities.bugs.list.filter((bug) => !bug.resolved);
 // the reducer can provide some predefined selectors of data
-// export const getUnresolvedBugs = (state) => state.entities.bugs.filter((bug) => !bug.resolved);
+// export const getUnresolvedBugs = (state) => state.entities.bugs.list.filter((bug) => !bug.resolved);
 
 // Better with Memoization if data has not changed
 // bugs => get unresolved bugs from cache
 export const getUnresolvedBugs = createSelector(
   (state) => state.entities.bugs,
   (bugs) =>
-    bugs.filter((bug) => {
+    bugs.list.filter((bug) => {
       console.log('function executed');
       return !bug.resolved;
     })
@@ -54,5 +62,5 @@ export const getUnresolvedBugs = createSelector(
 export const getBugByUser = (userId) =>
   createSelector(
     (state) => state.entities.bugs,
-    (bugs) => bugs.filter((bug) => bug.userId === userId)
+    (bugs) => bugs.list.filter((bug) => bug.userId === userId)
   );
